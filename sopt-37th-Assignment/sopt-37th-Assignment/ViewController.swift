29c06f5f80a9037498fdce865eaa26318d47ce75
Yeonnies <eliyeon16@gmail.com>
2025-10-28 14:23:31 +0900

    [feat] 프로젝트 생성

---
+++ sopt-37th-Assignment/sopt-37th-Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sopt-37th-Assignment
+//
+//  Created by 김나연 on 10/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
