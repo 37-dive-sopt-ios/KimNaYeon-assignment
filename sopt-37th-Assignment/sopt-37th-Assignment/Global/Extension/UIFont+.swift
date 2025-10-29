036ba80c5f0970d751f00469a4dc908b81cc5547
Yeonnies <eliyeon16@gmail.com>
2025-10-29 19:21:42 +0900

    [fix] 폰트 익스텐션 수정

--- sopt-37th-Assignment/sopt-37th-Assignment/Global/Extension/UIFont+.swift
+++ sopt-37th-Assignment/sopt-37th-Assignment/Global/Extension/UIFont+.swift
@@ -8,43 +8,9 @@
 import UIKit
 
 extension UIFont {
-
-    enum Typography {
-        case head_b_24
-        case head_b_18
-        case title_sb_18
-        case body_r_14
-        case caption_r_10
-
-        var font: UIFont {
-            switch self {
-            case .head_b_24:
-                return UIFont(name: "Pretendard-Bold", size: 24)!
-            case .head_b_18:
-                return UIFont(name: "Pretendard-Bold", size: 18)!
-            case .title_sb_18:
-                return UIFont(name: "Pretendard-SemiBold", size: 18)!
-            case .body_r_14:
-                return UIFont(name: "Pretendard-Regular", size: 14)!
-            case .caption_r_10:
-                return UIFont(name: "Pretendard-Regular", size: 10)!
-            }
-        }
-
-        var letterSpacing: CGFloat {
-            switch self {
-            case .head_b_24, .head_b_18:
-                return -0.02 * font.pointSize
-            case .title_sb_18:
-                return -0.03 * font.pointSize
-            case .body_r_14, .caption_r_10:
-                return -0.04 * font.pointSize
-            }
-        }
-
-        var lineHeight: CGFloat {
-            return font.pointSize * 1.0
-        }
-    }
+    static let head_b_24 = UIFont(name: "Pretendard-Bold", size: 24)!
+    static let head_b_18 = UIFont(name: "Pretendard-Bold", size: 18)!
+    static let title_sb_18 = UIFont(name: "Pretendard-SemiBold", size: 18)!
+    static let body_r_14 = UIFont(name: "Pretendard-Regular", size: 14)!
+    static let caption_r_10 = UIFont(name: "Pretendard-Regular", size: 10)!
 }
-
