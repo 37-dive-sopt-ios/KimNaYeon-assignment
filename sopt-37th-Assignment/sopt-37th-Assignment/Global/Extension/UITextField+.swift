babedb9d5e0132caca7909af1ed17001ab9baae3
Yeonnies <eliyeon16@gmail.com>
2025-10-31 07:27:07 +0900

    [feat] 텍스트필드

---
+++ sopt-37th-Assignment/sopt-37th-Assignment/Global/Extension/UITextField+.swift
@@ -0,0 +1,23 @@
+//
+//  UITextField+.swift
+//  sopt-37th-Assignment
+//
+//  Created by 김나연 on 10/31/25.
+//
+
+import UIKit
+
+extension UITextField {
+    func addRightPadding(_ width: CGFloat = 10) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.rightView = paddingView
+        self.rightViewMode = ViewMode.always
+    }
+    
+    func setPlaceholder(color: UIColor) {
+        guard let string = self.placeholder else {
+            return
+        }
+        attributedPlaceholder = NSAttributedString(string: string, attributes: [.foregroundColor: color])
+    }
+}
